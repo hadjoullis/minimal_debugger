c6f976ca4afed7efab8312fa74a8e669f99b7074
hadjoullis <andreashadjoullis@pm.me>
2025-12-12 22:13:07 +0100

    Change to only test argv

--- tests/test-2.c
+++ tests/test-2.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
-void bar(void) {
-    puts("bar");
-}
-
-void foo(void) {
-    puts("foo");
-    bar();
-}
-
 int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         puts(argv[i]);
     }
-    foo();
     return 0;
 }
